3eb952789de4d6a5a6f688ac3a7972235aaa6d0c
nghiaaaaa <idnpnlol@gmail.com>
2021-06-13 17:49:06 +0700

    layout menu

---
+++ app/src/main/java/com/example/v_school/Activity_7.java
@@ -0,0 +1,14 @@
+package com.example.v_school;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_7);
+    }
+}
